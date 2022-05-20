55f6c6a70f7a5ae5bdd472fb6908396c7b48bbf1
wangzi <623585585@qq.com>
2022-05-20 23:20:39 +0800

    update

--- src/cnn_stanfordcars.sh
+++ src/cnn_stanfordcars.sh
@@ -6,7 +6,7 @@ python train.py \
 --classes_per_it_tr 20 \
 --num_support_tr 5 \
 --num_query_tr 15 \
---classes_per_it_val 16 \
+--classes_per_it_val 20 \
 --num_support_val 5 \
 --num_query_val 15 \
 --height 128 \
