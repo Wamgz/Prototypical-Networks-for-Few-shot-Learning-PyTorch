fc540ac7144a97e034aa4901a01d7d9a1a43dfb9
wangzi <623585585@qq.com>
2022-06-13 11:28:53 +0800

    update

--- src/train.py
+++ src/train.py
@@ -185,7 +185,6 @@ def train(opt, tr_dataloader, model, optim, lr_scheduler, val_dataloader=None):
                                 n_support=opt.num_support_tr,
                                 n_query=opt.num_query_tr,
                                 dist=opt.dist)
-            logger.info('loss: {}'.format(loss))
 
             loss.backward()  # tensor(254.0303, grad_fn=<NegBackward0>)
             optim.step()
