6551e7703244774dedf32e39fbca69c24c72d659
wangzi <623585585@qq.com>
2022-06-13 22:31:53 +0800

    update

--- src/run_script/vit_miniimagenet.sh
+++ src/run_script/vit_miniimagenet.sh
@@ -4,14 +4,14 @@ python ../train.py \
 --epochs 1000 \
 --model_name vit \
 --dataset_name miniImagenet \
---classes_per_it_tr 5 \
+--classes_per_it_tr 16 \
 --num_support_tr 5 \
 --num_query_tr 15 \
---classes_per_it_val 5 \
+--classes_per_it_val 16 \
 --num_support_val 5 \
 --num_query_val 15 \
 --height 96 \
 --width 96 \
 --iterations 100 \
 --learning_rate 0.001 \
---cuda 3
+--cuda 2
