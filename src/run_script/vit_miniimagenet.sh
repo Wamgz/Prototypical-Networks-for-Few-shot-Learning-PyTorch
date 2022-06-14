bab7812e9b3f000ffcc2f525aa93ade9937ba095
wangzi <623585585@qq.com>
2022-06-14 12:33:10 +0800

    update

--- src/run_script/vit_miniimagenet.sh
+++ src/run_script/vit_miniimagenet.sh
@@ -4,10 +4,10 @@ python ../train.py \
 --epochs 1000 \
 --model_name vit \
 --dataset_name miniImagenet \
---classes_per_it_tr 16 \
+--classes_per_it_tr 5 \
 --num_support_tr 5 \
 --num_query_tr 15 \
---classes_per_it_val 16 \
+--classes_per_it_val 5 \
 --num_support_val 5 \
 --num_query_val 15 \
 --height 96 \
