7cf28ee31c20138d7746fb42795fbeaa1d748cea
wangzi <623585585@qq.com>
2022-06-21 00:41:23 +0800

    修改日志

--- src/prototypical_loss.py
+++ src/prototypical_loss.py
@@ -105,7 +105,6 @@ def prototypical_loss(model_outputs, labels, n_support, n_query, classes_dict, d
         model_outputs = mlp(model_outputs)
         classification_hat = F.log_softmax(model_outputs, -1)
         x_entropy = F.cross_entropy(classification_hat, labels)
-        logger.info('loss: {}, x_entropy: {}'.format(loss_val, x_entropy))
     return loss_val, scale * x_entropy,  acc_val
 
 
