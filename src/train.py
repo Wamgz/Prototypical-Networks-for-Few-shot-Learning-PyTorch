d5d8233abac2a4c7d8e1665fc75ff100727c771d
wangzi <623585585@qq.com>
2022-06-25 17:54:57 +0800

    config: lr调整

--- src/train.py
+++ src/train.py
@@ -144,8 +144,8 @@ def init_lr_scheduler(opt, optim):
     Initialize the learning rate scheduler
     '''
     # warm_up_with_cosine_lr
-    warm_up_with_cosine_lr = lambda epoch: (epoch + 1) / opt.warm_up_epochs if epoch <= opt.warm_up_epochs else 0.5 * (
-                math.cos((epoch + 1 - opt.warm_up_epochs) / (opt.epochs + 1 - opt.warm_up_epochs) * math.pi) + 1)
+    warm_up_with_cosine_lr = lambda epoch: (epoch + 1) / opt.warm_up_epochs if epoch <= opt.warm_up_epochs else 0.1 * (
+                math.cos((epoch + 1 - opt.warm_up_epochs) / (opt.epochs + 1 - opt.warm_up_epochs) * math.pi))
     return torch.optim.lr_scheduler.LambdaLR(optim, lr_lambda=warm_up_with_cosine_lr)
     # return torch.optim.lr_scheduler.StepLR(optimizer=optim,
     #                                        gamma=opt.lr_scheduler_gamma,
